c183c8af712fcbbfcb1a303362fee413e417a04a
Kelsey Low <kklowlow@gmail.com>
2019-07-10 23:03:03 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     return redirect_to(controller: 'sessions', action: 'new') unless @user.save
+    session[:user_id] = @user.id
   end
 
   private
